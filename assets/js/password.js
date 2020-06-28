8b3e445363a4e38d6aff337f5e43fc146e7643f2
Ryan Cooper <rjcooper32@gmail.com>
2020-06-28 14:10:30 +1200

    add password page

--- assets/js/password.js
+++ assets/js/password.js
@@ -3,7 +3,7 @@ function TheLogin() {
 var password = 'harnish';
 
 if (this.document.login.pass.value == password) {
-  top.location.href="index.html";
+  top.location.href="home.html";
 }
 else {
   window.alert("Incorrect password, fuck off.");
